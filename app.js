871026f9ed2c2cea5dc38c8eb77767ec58b838c2
nicolas <nicolas-dutertre@bbox.fr>
2023-03-13 13:09:12 +0100

    Gradient Box

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // start JS
+
+const gradientBox = document.querySelector(".gradient-box");
+const colorInputs = document.querySelectorAll(".colors input");
+
+const generateGradient = () =>{
+    const gradient = `linear-gradient(to top left, ${colorInputs[0].value}, ${(colorInputs[1].value)})`;
+    gradientBox.style.background = gradient;
+    console.log(gradient);
+}
+
+colorInputs.forEach(input =>{
+    input.addEventListener("input", generateGradient);
+});
