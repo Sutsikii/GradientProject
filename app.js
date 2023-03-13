54eb529ee7d2807ceb7fd699127cd78cb6d14567
nicolas <nicolas-dutertre@bbox.fr>
2023-03-13 13:16:23 +0100

    Textarea

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 // start JS
 
 const gradientBox = document.querySelector(".gradient-box");
+const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
+const textarea = document.querySelector("textarea");
 
 const generateGradient = () =>{
-    const gradient = `linear-gradient(to top left, ${colorInputs[0].value}, ${(colorInputs[1].value)})`;
+    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${(colorInputs[1].value)})`;
     gradientBox.style.background = gradient;
-    console.log(gradient);
+    textarea.value = `background: ${gradient};`;
 }
 
 colorInputs.forEach(input =>{
     input.addEventListener("input", generateGradient);
 });
+
+selectMenu.addEventListener("change", generateGradient);
+
+generateGradient;
